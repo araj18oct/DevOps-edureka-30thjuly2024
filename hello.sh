cf1c00ca2b9df537ef99391f927f21f5f86d82ab
edureka <ashu61meera@gmail.com>
2024-07-30 08:00:53 +0000

    third commit

--- hello.sh
+++ hello.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# Declaration of variables
-name=Tom
-age=12
-# Displaying variables
-echo $name $age
+
+# Define the file path and permissions
+FILE_PATH="/tmp/file.txt"  # Change this to your desired path
+PERMISSIONS=644  # Change this to your desired permissions
+
+# Create the file
+touch "$FILE_PATH"
+
+# Set the permissions
+chmod "$PERMISSIONS" "$FILE_PATH"
+
+echo "File created at $FILE_PATH with permissions $PERMISSIONS"
